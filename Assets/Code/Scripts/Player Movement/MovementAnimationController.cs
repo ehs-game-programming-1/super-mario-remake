de0176829de0729c52ae5905cb47e07c4e29c1b2
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2025-01-14 10:37:48 +0100

    Added simple animation controls

--- Assets/Code/Scripts/Player Movement/MovementAnimationController.cs
+++ Assets/Code/Scripts/Player Movement/MovementAnimationController.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class MovementAnimationController : MonoBehaviour
 {
-    
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        
+        animator.SetFloat("Movement", Mathf.Abs(movement));
+    }
 }
