466e83ba2aebcf4c59982f417bc95c05567c41db
epoHless <kevinsalimbeni@gmail.com>
2025-01-28 11:47:32 +0100

    Added Goomba & Player Death

--- Assets/Code/Scripts/Death/DeathAnimation.cs
+++ Assets/Code/Scripts/Death/DeathAnimation.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class DeathAnimation : MonoBehaviour
 {
     private DeathComponent _deathComponent;
+    private Animator _animator;
 
-    private void Awake() => _deathComponent = GetComponent<DeathComponent>();
+    private void Awake()
+    {
+        _deathComponent = GetComponent< DeathComponent >();
+        _animator = GetComponentInChildren< Animator >();
+    }
 
     private void OnEnable() => _deathComponent.OnDeath += PlayAnimation;
     private void OnDisable() => _deathComponent.OnDeath -= PlayAnimation;
 
-    void PlayAnimation() 
+    protected virtual void PlayAnimation() 
     {
-        
+        _animator.SetBool("IsDead", true);
     }
 }
--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [ field: SerializeField ] public MovementAnimationController AnimationController { get; private set; }
     [ field: SerializeField ] public Shooting Shooting { get; private set; }
     [ field: SerializeField ] public ScoreManager ScoreManager { get; private set; }
+    [ field: SerializeField ] public DeathComponent DeathComponent { get; private set; }
     
     [ field: SerializeField ] public Powerup ActivePowerup { get; set; }
     
@@ -14,5 +15,20 @@ public class PlayerController : MonoBehaviour
 
     private void Awake() => BoxCollider2D = GetComponent< BoxCollider2D >();
     private void Start() => ActivePowerup.Consume(this);
+
+    private void OnEnable() => DeathComponent.OnDeath += DisableScripts;
+    private void OnDisable() => DeathComponent.OnDeath -= DisableScripts;
+
+    private void DisableScripts()
+    {
+        Movement.AddForce(Vector2.up, 7, 1, ForceMode2D.Impulse);
+        
+        Movement.enabled = false;
+        Shooting.enabled = false;
+        AnimationController.enabled = false;
+
+        BoxCollider2D.enabled = false;
+    }
+
     public bool IsHigherPriority(Powerup powerup) => ActivePowerup.Priority > powerup.Priority;
 }
