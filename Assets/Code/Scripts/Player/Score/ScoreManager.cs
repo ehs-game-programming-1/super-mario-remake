5e41320ca95b8469975774e44542bf752588dd25
epoHless <kevinsalimbeni@gmail.com>
2025-01-28 12:11:01 +0100

    Changed ScoreManager to use events

--- Assets/Code/Scripts/Player/Score/ScoreManager.cs
+++ Assets/Code/Scripts/Player/Score/ScoreManager.cs
@@ -1,13 +1,28 @@
+using System;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
     [ SerializeField ] private ScoreUI ui;
-    [ SerializeField ] private int score;
+    [ field: SerializeField ] public Score Score { get; private set; }
 
+    private void Awake() => Score = new Score();
+
+    private void OnEnable() => Score.OnScoreChanged += ui.UpdateText;
+    private void OnDisable() => Score.OnScoreChanged -= ui.UpdateText;
+}
+
+[System.Serializable]
+public class Score
+{
+    [SerializeField] private int currentScore = 0;
+
+    public event Action< int > OnScoreChanged;
+    
     public void AddScore( int count )
     {
-        score += count;
-        ui.UpdateText(score);
+        currentScore += count;
+        
+        OnScoreChanged?.Invoke(currentScore);
     }
 }
