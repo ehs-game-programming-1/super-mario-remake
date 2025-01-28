2ab26456219d6d3a82cc7736e4d9cd3072f6a013
Lodo Marco <marco.lodo1010@gmail.com>
2025-01-28 12:16:15 +0100

    Added Koopa & animations

--- Assets/Code/Scripts/Death/DeathAnimation.cs
+++ Assets/Code/Scripts/Death/DeathAnimation.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class DeathAnimation : MonoBehaviour
 {
     private DeathComponent _deathComponent;
+    private Animator _animator;
 
-    private void Awake() => _deathComponent = GetComponent<DeathComponent>();
+    private void Awake()
+    {
+        _deathComponent = GetComponent<DeathComponent>();
+        _animator = GetComponentInChildren<Animator>();
+    }
 
     private void OnEnable() => _deathComponent.OnDeath += PlayAnimation;
     private void OnDisable() => _deathComponent.OnDeath -= PlayAnimation;
 
     void PlayAnimation() 
     {
-        
+        //if ( _animator.GetBool("IsAlive") )  
+        _animator.SetBool("IsAlive", false);
     }
 }
